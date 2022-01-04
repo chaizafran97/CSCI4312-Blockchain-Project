0393ed25e27690cbef40202480731477b6a9e43d
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2022-01-04 20:24:12 +0800

    Update Functions.sol

--- contracts/Functions.sol
+++ contracts/Functions.sol
@@ -1,40 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-enum State{
-	OrderCreated,			//0
-	PassedToLocalCustoms,	//1
-	Shipping,				//2
-	Delivered				//3
-}
+contract SupplyChain{
+	address cargoAddress;
 
-struct Cargo{
-	uint sku; //can be used as ID too
-	address sellerID;
-	address buyerID;
-	string buyerAddress;
-	string cargoDetails;
-	uint price;
-	State cargoState;
-}
+	enum State{
+		OrderCreated,			//0
+		PassedToLocalCustoms,	//1
+		Shipping,				//2
+		Delivered				//3
+	}
 
-//1st thing in supplychain
-function createOrder(){
-    address sellerID;
-    
-}
+	struct Role{
+		bool seller;
+		bool buyer;
+		bool customs;
+		bool courier;
+	}
 
-//function that will be used when changing ownership
-function moveCargo(){
+	struct Cargo{
+		uint sku;
+		address sellerID;
+		uint price;
+		string cargoDetails;
+	}
 
-}
+	//adding cargo for initial use
+    function createCargo(
+		uint _sku, 
+		address _sellerID, 
+		uint _price, 
+		string memory _cargoDetails) 
+		public {
+		Cargo.sku = _sku;
+		Cargo.sellerID = _sellerID;
+		Cargo.price = _price;
+		Cargo.cargoDetails = _cargoDetails;
+	}
 
-//function that allows customs to approve or deny the cargo
-function customsApproval(){
+	//1st thing in supplychain
+	function createOrder() public{
+		address sellerID;
+		
+	}
 
-}
+	//function that will be used when changing ownership
+	function moveCargo() public {
 
-//function for the buyer to approve of the product
-function buyerConfirmation(){
+	}
 
+	//function that allows customs to approve or deny the cargo
+	function customsApproval() public{
+
+	}
+
+	//function for the buyer to approve of the product
+	function buyerConfirmation() public {
+
+	}
 }
