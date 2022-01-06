b0667c8ff1577bc83609aad8102af8d239f28f17
chaizafran97 <82008745+chaizafran97@users.noreply.github.com>
2022-01-06 16:49:04 +0800

    Final changes before adding more complex functions

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -1,89 +1,62 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.5.0;
 
-contract SupplyChain{
+contract NewSupplyChain {
 
-	address cargoAddress;
-	mapping(address => uint256) private currentOwner;
-	uint256 sku;
-	string sellerID;
-	uint256 price;
-	string cargoDetails;
-	State cargoState;
-
-	enum State{
-		WithSeller,				//0
-		OrderCreated,			//1
-		PassedToLocalCustoms,	//2
-		Shipping,				//3
-		Delivered				//4
-	}
-
-	struct Cargo{
-		uint256 sku;
-		string sellerID;
-		uint256 price;
-		string cargoDetails;
-		State cargoState;
-		string buyer;
-	}
-
-	Cargo[] public cargo;
-	mapping(uint256 => string) public cargoToOwner;
-
-	//adding cargo for initial use
-    function createCargo(
-		uint256 _sku,
-		string memory _sellerID,
-		uint256 _price,
-		string memory _cargoDetails
-	) public {
-		sku = _sku;
-		sellerID = _sellerID;
-		price = _price;
-		cargoDetails = _cargoDetails;
-		cargoState = State.WithSeller;
-	}
-
-	//Order Created
-	function createOrder(uint256 _sku, string memory _buyer) public{
-		cargo[_sku].cargoState = State.OrderCreated;
-		cargo.push(Cargo.buyer(_buyer));
-
-	}
-
-	//function that will be used when passed to courier
-	function moveCargoToCourier(uint256 _sku) public {
-		cargo[_sku].cargoState = State.Shipping;
-	}
-
-	//function that will be used when passed to courier
-	function moveCargoToCustoms(uint256 _sku) public {
-		cargo[_sku].cargoState = State.PassedToLocalCustoms;
-	}
-
-	//function that will be used when passed to courier
-	function moveCargoToBuyer(uint256 _sku) public {
-		cargo[_sku].cargoState = State.Delivered;
-	}
-	    
-    function hasSellerRole( address _account) public view returns (bool){
-        require(_account != address(0));
-        return Role[_account].seller;
+   struct cargo{
+    uint256 cargoID;
+    uint256 cargoPrice;
+    string cargoDetails;
+    string cargoCoordinates;
+	string buyer;
+	string cargoState;
     }
 
-    function hasBuyerRole( address _account) public view returns (bool){
-        require(_account != address(1));
-        return Role[_account].buyer;
+    address owner;
+    cargo[] public Cargo;
+
+    event cargoBuy(uint256 temp);
+    event CargoMovedToCourier(uint256 temp);
+    event CargoMovedToCustoms(uint256 temp);
+    event CargoDeliveredToBuyer(uint256 temp);
+
+    constructor() public {
+        owner = msg.sender;
     }
 
-    function hasCourierRole( address _account) public view returns (bool){
-        require(_account != address(2));
-        return Role[_account].courier;
+	//initialize cargo, buyer can be left as N/a yet
+    function initializeCargo(
+        uint256 cargoPrice,
+        string memory cargoDetails,
+        string memory cargoCoordinates,
+		string memory buyer
+    ) public {
+		uint cargoID = 0;
+		string memory cargoState = "With Seller";
+      	Cargo.push(cargo(cargoID,cargoPrice,cargoDetails,cargoCoordinates,buyer,cargoState));
+	 	cargoID++;
     }
 
-    function hasCustomsRole( address _account) public view returns (bool){
-        require(_account != address(3));
-        return Role[_account].customs;
+	function cargoBought(uint256 _id, string memory _temp) public {
+		Cargo[_id].buyer = _temp;
+		Cargo[_id].cargoState = "Order Created";
+	}
+
+	function cargoShipped(uint256 _id) public {
+		Cargo[_id].cargoState = "Order currently shipping";
+	}
+
+	function cargoReceieved(uint256 _id) public {
+		Cargo[_id].cargoState = "Buyer has received";
+	}
+
+	//meant for testing if we can see the cargo
+	function viewCargo(uint256 _temp) public view 
+	returns(uint256, uint256, string memory, string memory, string memory){
+	return(Cargo[_temp].cargoID, Cargo[_temp].cargoPrice, Cargo[_temp].cargoDetails,
+	Cargo[_temp].cargoCoordinates, Cargo[_temp].buyer);
+	}
+
+
+
     }
-}
